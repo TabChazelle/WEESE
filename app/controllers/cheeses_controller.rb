146de0946bffd2f8d09ef7b04a350deea6a132db
Brandon Schneider <bcschnei87@gmail.com>
2024-02-05 07:48:13 -0800

    cheese controller

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,7 +1,27 @@
 class CheesesController < ApplicationController
+  before_action :set_cheese, only: [:show]
+  def new
+    @cheese = Cheese.new
+  end
+
   def index
+    @cheeses = Cheese.all
+    if params[:query].present?
+      sql_subquery = "name ILIKE :query OR type ILIKE :query OR country ILIKE :query"
+      @cheeses = @cheeses.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 
   def show
+  end
+
+  private
+
+  def set_cheese
+    @cheese = Cheese.find(params[:id])
+  end
+
+  def cheese_params
+    params.require(:cheese).permit(:name, :type, :country, :region, :milk, :description)
   end
 end
