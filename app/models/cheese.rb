dc195700c9144ec7dc3392f3611df84c05926a4d
Kai Tassie <argentiumkai@gmail.com>
2024-02-05 14:51:28 +0000

    Spelling error change

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,5 +1,5 @@
 class Cheese < ApplicationRecord
   has_many :pairing_tables
   has_many :wines, through: :pairing_tables
-  validates :name, :type, :country, :milk_type, presence: true
+  validates :name, :type, :country, :milk, presence: true
 end
