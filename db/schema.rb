fd9c861858d9a8bd2a0ce786e5e703d6278cb439
Kai Tassie <argentiumkai@gmail.com>
2024-02-06 11:35:33 +0000

    Seed file added, type renamed to softness in migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_174258) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_174260) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cheeses", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "softness"
     t.string "country"
     t.string "region"
     t.string "milk"
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_174258) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
 end
