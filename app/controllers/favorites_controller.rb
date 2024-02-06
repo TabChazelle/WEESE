486919d285501dfc20c2b56b0f555790f020f053
tabatha chazelle <tab_cha@yahoo.fr>
2024-02-06 14:12:52 +0100

    starting to look like something

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
   def index
     @favorite_wines = current_user.favorite_wines
     @favorite_cheeses = current_user.favorite_cheeses
+    @favorite_pairings = current_user.favorite_pairings
   end
-
 
 end
