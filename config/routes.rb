cb20cc06f51831ebf0fcf86f92099dce02de5c1f
tabatha chazelle <tab_cha@yahoo.fr>
2024-02-07 14:20:03 +0100

    made the favorite toggle work and display too

--- config/routes.rb
+++ config/routes.rb
@@ -2,29 +2,29 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :wines, only: [:index, :show]
+  resources :wines do
+    resources :favorite, only: [:index, :create, :destroy]
+    member do
+      get 'toggle_favorite', to: 'wines#toggle_favorite'
+    end
+  end
+
   resources :cheeses, only: [:index, :show]
   resources :pairings, only: [:index, :create, :destroy, :new, :show] do
-  resources :reviews, only: [:create]
-      end
+    resources :reviews, only: [:create]
+  end
 
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get "favorites", to: "users#favorites"
+    end
+  end
 
-      resources :wines do
-        resources :favorites, only: [:index, :create, :destroy]
-      member do
-        post :toggle_favorite, to: 'wines#toggle_favorite'
-        end
-      end
-
-  resources :openai, only: [:show]
-  resources :openai, only: [:create]
-
+  resources :openai, only: [:show, :create]
   resources :paths, only: [:index]
   resources :rails_health, only: [:show], path: 'up'
-  resources :users, only: [], path: 'profile'
 
   # Define a route for the search functionality
   get 'search', to: 'search#index', as: 'search'
   get '/profile', to: 'users#profile', as: 'profile'
-
 end
