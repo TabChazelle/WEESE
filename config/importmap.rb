0307c5f7f4ee8cd5451fb80ae0946fb955ca80a9
Brandon Schneider <bcschnei87@gmail.com>
2024-02-07 07:16:17 -0800

    review page and star rating

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,15 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "https://cdn.skypack.dev/@hotwired/turbo-rails", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# pin "application"
+# pin "@hotwired/turbo-rails", to: "https://cdn.skypack.dev/@hotwired/turbo-rails", preload: true
+# pin "@hotwired/stimulus", to: "stimulus.min.js"
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "star-rating.js" # @4.3.0
+pin "star-rating.js", to: "https://cdn.jsdelivr.net/npm/star-rating.js@4.3.0/dist/star-rating.esm.js"
+
