6d270027431176117a4e4247cc62df73d1151831
Kai Tassie <argentiumkai@gmail.com>
2024-02-07 11:27:38 +0000

    Turning the OpenAI into a service Object

--- app/controllers/openai_controller.rb
+++ app/controllers/openai_controller.rb
@@ -1,7 +1,6 @@
 class OpenaiController < ApplicationController
   def create
     @openai = OpenaiService.new(params[:openai][:prompt]).call
-
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: turbo_stream.replace('openai_response', partial: 'response', locals: { openai: @openai })
