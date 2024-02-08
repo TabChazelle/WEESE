210e2ce399c428aeb37382ee5570b06a2059f58f
Kai Tassie <argentiumkai@gmail.com>
2024-02-08 15:44:20 +0000

    Database insertion via AI WORKING, check for duplicates, rollback if duplicated

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -1,12 +1,12 @@
 class OpenaiService
-  HARD_CODED_PROMPT = "I'm going to give you a name of either a cheese or a wine. Your task is to respond in English with a single brand name that pairs well with the given input. If I give you the name of a cheese, you should respond with the name of a wine that pairs well with that cheese. If I give you the name of a wine, you should respond with the name of a cheese that pairs well with that wine. The response should be in the format 'BrandName, a type of Category'.
+  HARD_CODED_PROMPT = "I'm going to give you a name of either a cheese or a wine. Your task is to respond in English with a single brand name that pairs well with the given input. If I give you the name of a cheese, you should respond with the name of a wine that pairs well with that cheese. If I give you the name of a wine, you should respond with the name of a cheese that pairs well with that wine. The response should be in the format 'BrandName, Category'.
 
   For example:
   Input: Cheddar
-  Response: Cabernet Sauvignon, a type of wine
+  Cabernet Sauvignon Wine
 
   Input: Chardonnay
-  Response: Brie, a type of cheese
+  Brie Cheese
 
   Input: "
 
@@ -46,12 +46,17 @@ class OpenaiService
     if response['choices'] && response['choices'].first['message'] && response['choices'].first['message']['content']
       response_content = response['choices'].first['message']['content'].strip
       puts "API response content: #{response_content}" # This will log the response content in the server console
-      if response_content.downcase.include?('cheese')
+      words = response_content.split
+      last_word = words.pop.downcase
+      response_content = words.join(' ')
+      if last_word == 'cheese'
         # Save it to the cheese table
         Cheese.create(name: response_content)
-      else
+      elsif last_word == 'wine'
         # Save it to the wines table
         Wine.create(name: response_content)
+      else
+        puts "The last word is neither 'cheese' nor 'wine'."
       end
     else
       "There has been an error processing the response please try again"
