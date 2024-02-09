cbda57e9bc313c0089f2a1f389578fef61dcf964
Brandon Schneider <bcschnei87@gmail.com>
2024-02-09 02:05:06 -0800

    font change

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_104354) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_144736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +52,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_104354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+  end
+
+  create_table "favorite_favoritables", force: :cascade do |t|
+    t.string "favoritor_type", null: false
+    t.bigint "favoritor_id", null: false
+    t.string "favorite_type", null: false
+    t.bigint "favorite_id", null: false
+    t.string "scope"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["favorite_type", "favorite_id"], name: "index_favorite_favoritables_on_favorite"
+    t.index ["favoritor_type", "favoritor_id"], name: "index_favorite_favoritables_on_favoritor"
   end
 
   create_table "favorites", force: :cascade do |t|
