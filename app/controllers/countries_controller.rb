ec92323aed56cd1d8b3b4e10a8dd774d32439ffb
tabatha chazelle <tab_cha@yahoo.fr>
2024-02-13 17:00:16 +0100

    lot of visual aspects and one useless controller for now

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,14 +1,6 @@
 class CountriesController < ApplicationController
-  def show
-    @country = params[:id] # Assuming the country name is passed as the ID parameter
+  def show_cheeses
+    @country = params[:country_name]
     @cheeses = Cheese.where(country: @country)
-    @wines = Wine.where(country: @country)
-
-    # Determine the source based on the request or any other logic you have
-    @source = params[:source] || "cheese"
-
-    respond_to do |format|
-      format.html # Render HTML view
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,57 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#welcome"
   get 'home', to: 'pages#home', as: 'home'
 
   resources :wines do
     resources :favorites, only: [:index, :create, :destroy]
     member do
-      get 'toggle_favorite', to: 'wines#toggle_favorite'
+      get 'toggle_favorite'
     end
+    resources :reviews, only: [:create]
   end
 
   resources :cheeses do
     resources :favorites, only: [:index, :create, :destroy]
     member do
-      get 'toggle_favorite', to: 'cheeses#toggle_favorite'
+      get 'toggle_favorite'
     end
+    resources :reviews, only: [:create]
   end
 
   resources :pairings do
     resources :favorites, only: [:index, :create, :destroy]
     member do
-      get 'toggle_favorite', to: 'pairings#toggle_favorite'
+      get 'toggle_favorite'
     end
-  end
-
-  resources :reviews, only: [:create, :new, :destroy]
-
-  resources :pairings, only: [:index, :create, :destroy, :new, :show] do
     resources :reviews, only: [:new, :create]
-  end
-
-  resources :wines do
-    resources :reviews, only: [:create]
   end
 
   resources :users, only: [:show, :edit, :update] do
     member do
-      get "favorites", to: "users#favorites"
+      get "favorites"
     end
+    get 'edit_favorites', on: :member
   end
 
   resource :weesewizard, controller: 'openai', only: [:show, :create]
   post '/openai/openai_request', to: 'openai#openai_request'
 
-  resources :users do
-    get 'edit_favorites', on: :member
-  end
-
   resources :paths, only: [:index]
   resources :rails_health, only: [:show], path: 'up'
 
-
-  resources :countries, param: :country_name
-
-  # Define a route for the search functionality
   get 'search', to: 'search#index', as: 'search'
-  get '/profile', to: 'users#profile', as: 'profile'
+  get 'profile', to: 'users#profile', as: 'profile'
   get 'favorites', to: 'users#favorites', as: 'favorites'
   get 'edit_favorites', to: 'users#edit_favorites'
-
   get 'my_reviews', to: 'users#my_reviews', as: 'my_reviews'
 
+  # Route for showing cheeses by country
+  get 'cheeses/show_by_country/:country_name', to: 'cheeses#show_by_country', as: 'show_cheeses_by_country'
 
+  # Route for showing cheeses by country under the countries namespace
+  namespace :countries do
+    get 'cheeses/:country_name', to: 'cheeses#show_by_country', as: 'show_cheeses_by_country'
+  end
 end
